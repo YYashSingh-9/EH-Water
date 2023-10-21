979ad88380b828420c6947f12fdf406917b7bf8b
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-21 07:10:18 -0700

    Initial Commit

--- src/Components/BottomNavBar.jsx
+++ src/Components/BottomNavBar.jsx
@@ -5,22 +5,39 @@ import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import classes from "./BottomNavBar.module.css";
 
-export default function BottomNavigation() {
+export default function BottomNavBar() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        left: "15%",
+        backgroundColor: "red",
+        width: "70%",
+      }}
+    >
       <BottomNavigation
-        showLabels
+        showLabels={false}
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
+        className={classes.nav}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction
+          className={classes.bna}
+          icon={<RestoreIcon className={classes.icons} />}
+        />
+        <BottomNavigationAction
+          icon={<FavoriteIcon className={classes.icons} />}
+        />
+        <BottomNavigationAction
+          icon={<LocationOnIcon className={classes.icons} />}
+        />
       </BottomNavigation>
     </Box>
   );
--- src/ParentComponent.jsx
+++ src/ParentComponent.jsx
@@ -1,10 +1,13 @@
-import HeaderBar from "./Components/HeaderBar";
 import { Outlet } from "react-router-dom";
+import HeaderBar from "./Components/HeaderBar";
+import BottomNavBar from "./Components/BottomNavBar";
+
 const ParentComponent = () => {
   return (
     <>
       <HeaderBar />
       <Outlet />
+      <BottomNavBar />
     </>
   );
 };
