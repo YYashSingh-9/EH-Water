3322e001b22b5012b940e656aafdecd1d5e26baf
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-22 05:55:09 -0700

    logo added

--- src/Components/HeaderBar.jsx
+++ src/Components/HeaderBar.jsx
@@ -1,5 +1,5 @@
 import classes from "./HeaderBar.module.css";
-import { AppBar, Box } from "@mui/material";
+import { AppBar, Box, Grid } from "@mui/material";
 import Logo from "../assets/Images/finalLogo1.png";
 import g20Logo from "../assets/Images/g20.png";
 const HeaderBar = () => {
@@ -16,9 +16,23 @@ const HeaderBar = () => {
           className={classes.appbar}
         >
           <Box className={classes.logoDiv}>
-            <img src={Logo} />
-            <span className={classes.betaTag}>Beta</span>
-            <img src={g20Logo} className={classes.g20Img} />
+            <Grid
+              container
+              direction={"row"}
+              justifyContent={"flex-end"}
+              alignItems={"center"}
+              sx={{ maxHeight: "10rem" }}
+            >
+              <Grid item lg={8} md={8} sm={8} textAlign={"center"}>
+                <Box className={classes.logos}>
+                  <img src={Logo} />
+                  <span className={classes.betaTag}>Beta</span>
+                </Box>
+              </Grid>
+              <Grid item lg={4} md={4} sm={4} textAlign={"right"}>
+                <img src={g20Logo} className={classes.g20Img} />
+              </Grid>
+            </Grid>
           </Box>
         </AppBar>
       </Box>
