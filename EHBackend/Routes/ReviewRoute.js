bcbe451b1552834fae0797cd0a2cdba7b4ff2824
YYashSingh <yyashsngh@gmail.com>
2023-10-25 04:53:06 -0700

    api optimization

--- EHBackend/Routes/ReviewRoute.js
+++ EHBackend/Routes/ReviewRoute.js
@@ -3,10 +3,8 @@ const ReviewController = require("../Controllers/ReviewController");
 const AuthController = require("../Controllers/AuthController");
 const reviewRouter = express.Router();
 
+reviewRouter.route("/").get(ReviewController.getAllReviews);
 reviewRouter.use(AuthController.protect);
-reviewRouter
-  .route("/")
-  .get(ReviewController.getAllReviews)
-  .post(ReviewController.postOneReview);
+reviewRouter.route("/").post(ReviewController.postOneReview);
 
 module.exports = reviewRouter;
