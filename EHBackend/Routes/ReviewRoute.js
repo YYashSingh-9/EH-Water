c85e36a32ef1f05cb76c44680bccabc053d4ce8f
YYashSingh <yyashsngh@gmail.com>
2023-10-25 07:12:03 -0700

    API Finals

--- EHBackend/Routes/ReviewRoute.js
+++ EHBackend/Routes/ReviewRoute.js
@@ -3,8 +3,13 @@ const ReviewController = require("../Controllers/ReviewController");
 const AuthController = require("../Controllers/AuthController");
 const reviewRouter = express.Router();
 
-reviewRouter.route("/").get(ReviewController.getAllReviews);
+reviewRouter
+  .route("/getIssueReviews")
+  .get(ReviewController.id_value_changer, ReviewController.getYourReviews);
 reviewRouter.use(AuthController.protect);
-reviewRouter.route("/").post(ReviewController.postOneReview);
+reviewRouter
+  .route("/")
+  .post(ReviewController.postOneReview)
+  .get(ReviewController.getAllReviews);
 
 module.exports = reviewRouter;
