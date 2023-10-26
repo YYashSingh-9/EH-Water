f983e6a61e57036fc4447106e4c6f4c62b187e6e
YYashSingh-9 <yyashsngh@gmail.com>
2023-10-26 05:11:18 -0700

    more logic

--- src/Components/WholePages/UserPage.jsx
+++ src/Components/WholePages/UserPage.jsx
@@ -1,11 +1,17 @@
-import classes from "./User.module.css";
-import LogoutIcon from "@mui/icons-material/Logout";
-import EditIcon from "@mui/icons-material/Edit";
-import { Box, Grid, Button } from "@mui/material";
 import ProfileThreadComponent from "../ChildComponents/ProfileThreadComponent";
-import { useNavigate } from "react-router-dom";
-const UserPage = () => {
+import { useDispatch, useSelector } from "react-redux";
+import { sliceActions } from "../../Store/StoreSlice";
+import LogoutIcon from "@mui/icons-material/Logout";
+import LoginPage from "../AccountPages/LoginPage";
+import { Box, Grid, Button } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import { useActionData, useNavigate } from "react-router-dom";
+import classes from "./User.module.css";
+import { useEffect } from "react";
+
+const WhenLoggedIn = () => {
   const Navigate = useNavigate();
+
   return (
     <>
       <Box className={classes.mainUserDiv}>
@@ -48,6 +54,21 @@ const UserPage = () => {
       </Box>
     </>
   );
+};
+
+const UserPage = () => {
+  const loggedInState = useSelector((state) => state.firstSlice.loginState);
+  const dispatch = useDispatch();
+  const action_data = useActionData();
+  useEffect(() => {
+    dispatch(sliceActions.get_token_from_localStorage());
+    if (action_data) {
+      if (action_data.status === "success") {
+        dispatch(sliceActions.set_token_to_localStorage(action_data.data));
+      }
+    }
+  }, [action_data]);
+  return <>{loggedInState ? <WhenLoggedIn /> : <LoginPage />}</>;
 };
 
 export default UserPage;
